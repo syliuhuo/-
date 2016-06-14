bc4eefafff67cc314832954e907b8a6fdbc1d322
lizhiyuan <148334733@qq.com>
2016-06-14 15:51:27 +0800

    注册的最终版本

--- server.js
+++ server.js
@@ -38,26 +38,52 @@ app.use(express.static('www'));
 
 
 //用户post提交注册页面
+//这里面的意思是，如果用户发送了一个post请求，而这个post请求的地址是register，那么
+//我就用后边的这个回调函数来处理.回调函数中有两个参数,req请求.res返回
+//也就是说在这个回调函数，我既可以拿到请求里面的数据req，也可以返回给用户数据res.
 app.post('/register',function(req,res){
+    //req.query ,req.body拿到数据,get提交的数据req.query里面，如果是post提交,那么数据就是
+    //在req.body里面取得
     //首先我们先保存下用户的IP
+    //我在拿到数据之前，在这个body里面新增了两个属性，第一个属性是用户的IP，第二个属性是
+    //用户发送请求的当前时间，我也想记录一下，和数据放在一起，存到文件当中去.
     req.body.ip = req.ip;
     //再保存下日期
     req.body.time = new Date();
+    //取一下用户名 req.body.petname
+    //取一下密码 req.body.password
+    //取一下email req.body.email
+
+
     //发送函数
     function send(code,message){
         res.status(200).json({code,message});
     }
     //把数据保存在一个users/用户名命名.txt的文件中
+    //保存数据到文件的一个函数，将注册的用户名保存到users/用户名.txt文件当中去.
+    //这样，我就能知道哪些人注册了，我才可以进行登录的判断。
     function saveFile(){
-        //文件名
+        //首先，我要确定一下保存的文件目录是在users/用户名.txt文件当中去.
+        //以后，写这个文件路径的时候加`` ,先记住.
         var filename = `users/${req.body.petname}.txt`;
-        //先查看下文件名是否存在
+        //fs是不是专门来负责文件的读写的.
+        //fs.exists它是用来检测文件是否存在的这么一个函数.
+        //传递了检测文件的文件名，检测完文件名调用回调函数，结果就是exists
         fs.exists(filename,function(exists){
             if(exists){
                 //如果存在的话，在模态框中提示用户名已经存在了
+                //用户注册的信息，我查看一下，在这个文件中已经存在了，那么，我就返回给
+                //前端，告诉他，用户名已经存在了,前端呢，通过modal模态框输出结果。
                 send('error','用户名已经存在');
+                //res.status(200).json({code:"error",message:'用户名已经存在'});
             }else{
                 //如果不存在的话，保存一下
+                //如果文件名不存在，说明可以进行正常的注册啦。
+                //fs.appendFile是用来打开文件并写入的一个函数
+                //第一个参数，要写入的文件的名称
+                //第二个参数，要写入哪些数据
+                //第三个参数，回调函数当中，返回错误信息，err
+                //根据返回的信息，send不同的状态。
                 fs.appendFile(filename,JSON.stringify(req.body),function(err){
                     if(err){
                         send('error','系统错误');
@@ -65,7 +91,6 @@ app.post('/register',function(req,res){
                         send('success','注册成功');
                     }
                 })
-
             }
         })
     }
--- www/scripts/register.js
+++ www/scripts/register.js
@@ -4,19 +4,45 @@
 //我们要在这里面，将用户注册的信息发送给服务器
 //收集表单的数据
 
+
+
+//设置一下表单提交的事件，当我点击submit的时候，这个事件触发了,回调函数执行.
+
 $('form').submit(function(e){
     //第一步,阻止下默认行为
     //禁止表单的自动提交
+    //禁止了表单跳转的默认行为，默认的情况下，它会跳转到action属性的地址中去.
     e.preventDefault();
     //第二步，使用post提交，将表单的数据传递给node后台express脑子接收
+    //表单当中的所有的数据都已经放在了这个data里面.
+    //serialze收集一下表单上所有表单的数据.
     var data = $(this).serialize();
-    //第三步,使用post AJAX操作
-    $.post('/register',data,function(data){
-        $('.modal-body').text(data.message);
-        $('.modal').modal('show').on('hidden.bs.modal',function(){
-            if(data.code == 'success'){
-                location.href = 'signin.html';
-            }
+
+    if($('input[name=password]').val() === $('input[name=rpassword]').val()){
+        //第三步,使用post AJAX操作
+        //$.post后边跟三个参数，第一个参数是发送的地址，第二个参数是发送的数据,
+        //第三个参数是用来处理数据的这个回调函数.在这个回调函数当中，会返回三个数据
+        //status,reponse.... 在回调函数当中，我们可以接收到服务器给我们返回的数据内容
+        //也就说data是后端服务器返回给我们的数据.
+        console.log(data);
+        $.post('/register',data,function(data){
+            //data里面message是一个返回的信息,例如说注册成功啊，注册失败啦。
+            $('.modal-body').text(data.message);
+            //我们使用JS将带有返回信息的模态框打开
+            //hidden.bs.modal关闭弹出框的时候，执行一个回调函数
+            //当返回的code是success的时候，也就说，用户已经注册成功了，
+            //我们就跳转到signin.html页面当中去.
+            $('.modal').modal('show').on('hidden.bs.modal',function(){
+                if(data.code == 'success'){
+                    //当用户注册成功之后，跳转到signin.html页面当中去.
+                    location.href = 'signin.html';
+                }
+            })
         })
-    })
+
+    }else{
+        $('.modal-body').text('密码不一样');
+        $('.modal').modal('show');
+    }
+
 })
